111c7de364235d09e3afd48742c48419d349ad28
LiGu <89311746+gulideshanhe@users.noreply.github.com>
2024-11-20 16:23:32 +0800

    添加logging库，修复潜在的bug

--- 青果-登录.py
+++ 青果-登录.py
@@ -1,11 +1,13 @@
 import base64
 import hashlib
+import logging
 
 import requests
 from bs4 import BeautifulSoup
 
 import queryParams
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # http://t.csdnimg.cn/5hVov
 
@@ -28,7 +30,7 @@ class KINGOSOFT():
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0"
     }
         self.sessionID = self.session.get(f"{self.mainURL}/custom/js/GetKingoEncypt.jsp", headers= self.headers).cookies["JSESSIONID"]
-    
+
     def sign(self,DES_KEY: str = "93015171955021563496877", SIGN_TIME: str = "2024-07-02 17:17:17") -> bool:
         username = base64.b64encode(str(f"{self.username};;"+self.sessionID).encode('utf-8')).decode('utf-8')
         params = f"_u={username}&_p={md5(md5(self.password)+md5(""))}&randnumber=&isPasswordPolicy=1&txt_mm_expression=13&txt_mm_length=8&txt_mm_userzh=0&hid_flag=1&hidlag=1"
@@ -44,18 +46,19 @@ class KINGOSOFT():
             "ssessionid": self.sessionID
         }
         response = self.session.post(f"{self.mainURL}/cas/logon.action", headers= self.headers, data= date)
-        print(response.text)
+        logging.debug(response.text)
         if "操作成功!" in response.text:
-            print("登录cookie:", self.session.cookies["JSESSIONID"])
+            logging.debug("登录cookie:", self.session.cookies["JSESSIONID"])
             return True
         elif "账号或密码有误!" in response.text:
-            print("账号或密码有误!")
+            logging.error("账号或密码有误!")
             return False
         else:
             assert False, "意外错误，请检查代码！"
         
     def get_grade(self) -> list:
         data = {
+            # **改动后请对照网页表单修改，不然可能无法正确查询**
             "sjxz": "sjxz3", # sjxz1：查询入学以来成绩 sjxz2：查询学年成绩 sjxz3：查询学期成绩
             "ysyx": "yscj", #yscj：查询原始成绩 yxcj：查询有效成绩
             "zx": "1", # 主修是否查询 0：不查询 1：查询
@@ -75,13 +78,19 @@ class KINGOSOFT():
         response = self.session.post(f"{self.mainURL}/student/xscj.stuckcj_data.jsp", headers= self.headers, data= data)
         
         soup = BeautifulSoup(response.text, "html.parser")
-        table = soup.select("table")[-1].select("tr")
+        try:
+            table = soup.select("table")[-1].select("tr")
+        except IndexError:
+            logging.error("暂无成绩！")
+            table=[]
         class_grade = []
         for tr in table[1:]:
             td = tr.select("td")
-            class_grade.append((td[1].text, td[12].text))
+            try:
+                class_grade.append((td[1].text, td[12].text, td[8].text, td[9].text, td[10].text,))
+            except IndexError:
+                class_grade.append((td[1].text, td[8].text, "NULL", "NULL", "NULL",))
             # td[1]是课程名称，td[12]是综合成绩。8是平时成绩，9是期中成绩，10是期末成绩
-            # 可能某些学校设置只返回综合成绩，不公开平时成绩等，就需要**改动索引**
         return class_grade
             
 
@@ -91,10 +100,10 @@ def md5(text: str) -> str:
     return md5.hexdigest()
 
 
-if __name__ == '__main__':
-    kingosoft = KINGOSOFT("username", "password", "URL")
+if __name__ == "__main__":
+    kingosoft = KINGOSOFT("username", "password", "https://example")
     if kingosoft.sign():
         grade = kingosoft.get_grade()
-        print(grade)
+        logging.info(grade)
     else:
-        pass
+        logging.info("登录失败！")
